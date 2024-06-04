70a62ea18c303b01324000d21102d74de742632d
Cyril Hrubis <metan@ucw.cz>
2024-06-04 21:11:50 +0200

    Update to gfxprim changes.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- gplife.c
+++ gplife.c
@@ -354,8 +354,7 @@ int main(int argc, char *argv[])
 
 	if (view.pixmap) {
 		gp_widget_on_event_set(view.pixmap, pixmap_on_event, NULL);
-		gp_widget_event_unmask(view.pixmap, GP_WIDGET_EVENT_REDRAW);
-		gp_widget_event_unmask(view.pixmap, GP_WIDGET_EVENT_INPUT);
+		gp_widget_events_unmask(view.pixmap, GP_WIDGET_EVENT_INPUT | GP_WIDGET_EVENT_REDRAW);
 	}
 
 	w = gp_widget_by_cuid(uids, "w", GP_WIDGET_CLASS_INT);
